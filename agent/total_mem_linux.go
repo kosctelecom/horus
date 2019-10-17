67b2be02e22c93fd87eaf6bf296bd9cb5576f475
Vallimamod Abdullah <vallimamod.abdullah@kosc-telecom.fr>
2019-10-17 10:49:38 +0200

    limits syscall to get total memory to linux systems

---
+++ agent/total_mem_linux.go
@@ -0,0 +1,18 @@
+// +build linux
+
+package agent
+
+import (
+	"horus-core/log"
+	"syscall"
+)
+
+// sysTotalMemory returns the total system memory on linux.
+func sysTotalMemory() uint64 {
+	in := &syscall.Sysinfo_t{}
+	if err := syscall.Sysinfo(in); err != nil {
+		log.Errorf("sysTotalMemory: %v", err)
+		return 0
+	}
+	return uint64(in.Totalram) * uint64(in.Unit)
+}
