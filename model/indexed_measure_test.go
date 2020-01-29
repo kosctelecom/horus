4dc59e1bbede6a5f52d6ca90308d2bbe2a593e2a
Vallimamod Abdullah <vma@sip.solutions>
2020-01-29 18:22:19 +0100

    updates year in license headers

--- model/indexed_measure_test.go
+++ model/indexed_measure_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Kosc Telecom.
+// Copyright 2019-2020 Kosc Telecom.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
